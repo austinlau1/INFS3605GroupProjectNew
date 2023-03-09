1e875b50867936b5d8bf3cac743ac0edc0230ad6
Austin Lau <90658215+austinlau1@users.noreply.github.com>
2023-03-09 11:56:22 +1100

    Transferred all my files from previous repo

---
+++ app/src/main/java/com/example/infs3605groupprojectnew/LoginActivity.java
@@ -0,0 +1,129 @@
+package com.example.infs3605groupprojectnew;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity{
+
+    private static final String TAG = "LoginPage";
+    //    DBHelper DB;
+    private FirebaseAuth mAuth;
+    private ProgressDialog mLoadingProgressBar;
+    EditText user_email, user_passw;
+    Button btnForgetPW;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: Starting Launch");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+
+        user_email = findViewById(R.id.email_input);
+        user_passw = findViewById(R.id.pass_input);
+        Button registerbtn = findViewById(R.id.rego_button);
+        Button userloginbtn = findViewById(R.id.login_button);
+        mAuth = FirebaseAuth.getInstance();
+        mLoadingProgressBar = new ProgressDialog(LoginActivity.this);
+
+        //user sign up direction
+        registerbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+
+        //user forget password
+
+        btnForgetPW = findViewById(R.id.forgot_button);
+        btnForgetPW.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, ForgotPassword.class));
+            }
+        });
+
+        userloginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String inputEmail = user_email.getText().toString().trim();
+                final String inputPassword = user_passw.getText().toString().trim();
+                loginWithEmailAndPassword(inputEmail, inputPassword);
+            }
+        });
+
+    }
+
+    private void loginWithEmailAndPassword(String inputEmail, String inputPassword) {
+
+        if (validate()==false) {
+            return;
+        } else {
+
+            mLoadingProgressBar.setTitle("Login");
+            mLoadingProgressBar.setMessage("Checking your user details!");
+            mLoadingProgressBar.setCanceledOnTouchOutside(false);
+            mLoadingProgressBar.show();
+
+            //Authentication process
+            mAuth.signInWithEmailAndPassword(inputEmail, inputPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()) {
+                        mLoadingProgressBar.dismiss();
+                        Intent intent = new Intent(LoginActivity.this, WelcomeActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        intent.putExtra("email",inputEmail);
+                        startActivity(intent);
+
+                    } else {
+
+                        Toast.makeText(LoginActivity.this, "User Not Existed! Please Sign Up", Toast.LENGTH_LONG).show();
+
+                    }
+                }
+            });
+        }
+
+    }
+
+
+    private boolean validate() {
+        if (user_email.getText().toString().isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please enter your email", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        else if (user_passw.getText().toString().isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please enter password", Toast.LENGTH_SHORT).show();
+            return  false;
+        }
+        else{
+
+            user_email.setError(null);
+            user_passw.setError(null);
+            return true;
+        }
+    }
+}
+
+/**
+ https://www.youtube.com/watch?v=741QCymuky4
+ This is the resources used behind creating user login and user registration through firebase database
+ */
+
---
+++ app/src/main/java/com/example/infs3605groupprojectnew/RegisterActivity.java
@@ -0,0 +1,205 @@
+package com.example.infs3605groupprojectnew;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
+
+public class RegisterActivity extends AppCompatActivity{
+
+    EditText Inuser_name, Inuser_password, Inuser_email, Inuser_first_name, Inuser_last_name;
+    TextView btnAlready;
+    private FirebaseAuth mAuth;
+    private ProgressDialog mLoadingProgres;
+
+    private static final String TAG = "Registration Page";
+
+    UserDAO dao = new UserDAO();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: Starting Launch");
+        setTitle("Register User");
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_page);
+
+        Inuser_name = findViewById(R.id.username);
+        Inuser_password  = findViewById(R.id.password);
+        Inuser_email = findViewById(R.id.email);
+        Inuser_first_name = findViewById(R.id.first_name);
+        Inuser_last_name = findViewById(R.id.last_name);
+        mLoadingProgres = new ProgressDialog(RegisterActivity.this);
+        mAuth = FirebaseAuth.getInstance();
+
+
+        btnAlready = findViewById(R.id.have_acc_button);
+        btnAlready.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
+// adding user into the firebase database
+        Button rego_save = findViewById(R.id.register_button);
+        rego_save.setOnClickListener(v-> {
+
+            if(!validateUsername() |  !validateEmail() | !validateFirstName() | !validateLastName()) {
+                return;
+            }
+
+            String username = Inuser_name.getText().toString();
+            String password =  Inuser_password.getText().toString();
+            String email  =    Inuser_email.getText().toString();
+            String firstname = Inuser_first_name.getText().toString();
+            String lastname  = Inuser_last_name.getText().toString();
+
+            mLoadingProgres.setTitle("Registration");
+            mLoadingProgres.setMessage("Checking your login details!");
+            mLoadingProgres.setCanceledOnTouchOutside(false);
+            mLoadingProgres.show();
+
+            //Send email and password to firebase for authentication
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    // if the user has been registered successfully -> create a user object to store them in realtime database
+                    if(task.isSuccessful()){
+                        User user = new User(username, password, email, firstname, lastname);
+
+                        //send user info to real time database + get ID -> turns out the id for the registered user
+
+                        FirebaseDatabase.getInstance().getReference("Users").child(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid());
+                        dao.add(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(RegisterActivity.this,"User has been registered successfully",Toast.LENGTH_LONG).show();
+
+                                    mLoadingProgres.dismiss();
+                                }else{
+                                    Toast.makeText(RegisterActivity.this,"Failed to register! Please try again!",Toast.LENGTH_LONG).show();
+                                    mLoadingProgres.dismiss();
+                                }
+                            }
+                        });
+                    }
+                }
+            });
+        });
+    }
+
+    //method to validate user's input
+    private Boolean validateUsername(){
+        String val = Inuser_name.getText().toString();
+        String noWhiteSpace = "\\A\\w{4,20}\\z";
+        if (val.isEmpty()){
+            Inuser_name.setError("Field cannot be empty");
+            return false;
+
+        }
+        else if(!val.matches(noWhiteSpace)){
+            Inuser_name.setError("White Spaces are not allowed");
+            return false;
+
+        }
+        // pass the error when the first time the user entered
+        else{
+            Inuser_name.setError(null);
+//        user_name.setErrorEnabled(false);
+            return true;
+        }
+
+    }
+    private Boolean validatePassword() {
+        String val = Inuser_password.getText().toString();
+        String valPass = "^" +
+                " (.*[A-Z].*)" + //must have atleast one uppercase character
+//                        "(.*[@,#,$,%].*$)" + // at least 1 special character special character among @#$%"
+                "(.*[0-9].*)" ;// Password must have atleast one number
+
+        if (val.isEmpty()) {
+            Inuser_password.setError("Field cannot be empty");
+            return false;
+
+        } else if (!val.matches(valPass) && val.length() < 8) {
+            Inuser_password.setError("Password is too weak");
+            return false;
+        }
+        // pass the error when the first time the user entered
+        else {
+            Inuser_password.setError(null);
+            return true;
+        }
+    }
+
+    private Boolean validateEmail(){
+        String val = Inuser_email.getText().toString();
+        String emailPattern = "[a-zA-z0-9._-]+@[a-z]+\\.+[a-z]+";
+        if (val.isEmpty()){
+            Inuser_email.setError("Field cannot be empty");
+            return false;
+
+        }
+        else if(!val.matches(emailPattern)){
+            Inuser_email.setError("Invalid email address");
+            return false;
+
+        }
+        // pass the error when the first time the user entered
+        else{
+            Inuser_email.setError(null);
+            return true;
+        }
+
+
+    }
+    private Boolean validateFirstName(){
+        String val = Inuser_first_name.getText().toString();
+        if (val.isEmpty()){
+            Inuser_first_name.setError("Field cannot be empty");
+            return false;}
+        // pass the error when the first time the user entered
+        else{
+            Inuser_first_name.setError(null);
+            return true;
+        }
+    }
+    private Boolean validateLastName(){
+        String val = Inuser_last_name.getText().toString();
+        if (val.isEmpty()){
+            Inuser_last_name.setError("Field cannot be empty");
+            return false;}
+        // pass the error when the first time the user entered
+        else{
+            Inuser_last_name.setError(null);
+            return true;
+        }
+    }
+
+}
+// pass the user object and the test result is returned back which we can listen to callback success and fail
+
+//        dao.add(user).addOnSuccessListener(suc->{
+//                Toast.makeText(this, "Record is inserted", Toast.LENGTH_SHORT).show();
+//                }).addOnFailureListener(er->{
+//                Toast.makeText(this, "" +er.getMessage(),Toast.LENGTH_SHORT).show();
+//                });
+
