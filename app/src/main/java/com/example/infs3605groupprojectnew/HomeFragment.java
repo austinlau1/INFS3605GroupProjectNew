ac33e8c1e734397c62621736cc2d1757523d5055
RohitW1 <z5310030@ad.unsw.edu.au>
2023-03-14 07:01:23 +1100

    Changes

--- app/src/main/java/com/example/infs3605groupprojectnew/HomeFragment.java
+++ app/src/main/java/com/example/infs3605groupprojectnew/HomeFragment.java
@@ -1,27 +1,63 @@
 package com.example.infs3605groupprojectnew;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 import com.example.infs3605groupprojectnew.Quizzes.Quizzes;
 
 
 public class HomeFragment extends Fragment {
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView =  inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        //Intent intent = getActivity().getIntent();
+        //Boolean popup = intent.getBooleanExtra("popup", false);
+        //System.out.println(popup);
+
+        /*LayoutInflater inflater2 = (LayoutInflater)
+                getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater2.inflate(R.layout.popup_acknowledgement, container, false);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(rootView, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });*/
 
         Button toPlantsBtn = (Button) rootView.findViewById(R.id.toPlantsBtn);
 
@@ -42,6 +78,7 @@ public class HomeFragment extends Fragment {
                 startActivity(intent);
             }
         });
+
 
         return rootView;
     }
