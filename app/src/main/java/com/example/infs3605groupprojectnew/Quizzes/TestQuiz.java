66a7aef7aac5c5f226aede33d7c86053a66d3049
RohitW1 <z5310030@ad.unsw.edu.au>
2023-03-19 17:28:28 +1100

    Quiz updates

--- app/src/main/java/com/example/infs3605groupprojectnew/Quizzes/TestQuiz.java
+++ app/src/main/java/com/example/infs3605groupprojectnew/Quizzes/TestQuiz.java
@@ -5,15 +5,21 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.example.infs3605groupprojectnew.R;
+import com.github.jinatonic.confetti.ConfettiManager;
+import com.github.jinatonic.confetti.ConfettiView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -21,11 +27,16 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
 
 public class TestQuiz extends AppCompatActivity {
     private ArrayList<QuizQuestion> quizQuestions;
     private int currentQuestionIndex = 0;
     private int score = 0;
+    private CountDownTimer timer;
+    private AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +62,8 @@ public class TestQuiz extends AppCompatActivity {
                     quizQuestions.add(quizQuestion);
                 }
 
-                displayQuestion();
+                startTimer();
+
                 Button nextButton = findViewById(R.id.next_button);
                 nextButton.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -67,6 +79,27 @@ public class TestQuiz extends AppCompatActivity {
                 Log.e("QuizActivity", "Error getting questions from database", databaseError.toException());
             }
         });
+    }
+
+    private void startTimer() {
+        timer = new CountDownTimer(60000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                // Update the timer TextView every second
+                int secondsLeft = (int) millisUntilFinished / 1000;
+                String timeLeft = String.format(Locale.getDefault(), ":%02d", secondsLeft);
+                TextView tvTimer = findViewById(R.id.tv_timer);
+                //Change "0" if total time is changed
+                tvTimer.setText("0" + timeLeft);
+            }
+
+            @Override
+            public void onFinish() {
+                // Handle timer finish
+                showResult();
+            }
+        }.start();
+        displayQuestion();
     }
 
     private void displayQuestion() {
@@ -111,6 +144,9 @@ public class TestQuiz extends AppCompatActivity {
 
         currentQuestionIndex++;
 
+        // Clear the checked radio button
+        radioGroup.clearCheck();
+
         if (currentQuestionIndex >= quizQuestions.size()) {
             // Quiz is over
             showResult();
@@ -120,25 +156,78 @@ public class TestQuiz extends AppCompatActivity {
     }
 
     private void showResult() {
-        String resultMessage = "You scored " + score + " out of " + quizQuestions.size() + "!";
+
+        // Inflate the layout file for the "show result" screen
+        View resultView = getLayoutInflater().inflate(R.layout.quiz_result, null);
+
+        // Find the FrameLayout view in the layout
+        FrameLayout trophyFrameLayout = resultView.findViewById(R.id.trophyFrameLayout);
+
+        // Create an AlertDialog with the custom layout
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Quiz Result")
-                .setMessage(resultMessage)
-                .setPositiveButton("Restart", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        restartQuiz();
-                    }
-                })
-                .setNegativeButton("Exit", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        finish();
-                    }
-                });
+        builder.setView(resultView);
         AlertDialog dialog = builder.create();
         dialog.show();
+
+        // Create an ImageView and add it to the FrameLayout
+        ImageView trophyImageView = new ImageView(this);
+        trophyImageView.setImageResource(R.drawable.gold_trophy);
+        trophyFrameLayout.addView(trophyImageView);
+
+        // Create a ConfettiView and add it to the FrameLayout
+        ConfettiView confettiView = new ConfettiView(this, null);
+        trophyFrameLayout.addView(confettiView);
+
+        // Find any relevant views in the layout and update their values or add listeners
+        TextView scoreTextView = resultView.findViewById(R.id.scoreTextView);
+        scoreTextView.setText("You scored " + score + " out of " + quizQuestions.size() + "!");
+
+        Button restartButton = resultView.findViewById(R.id.restartButton);
+        restartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                restartQuiz();
+            }
+        });
+
+        Button exitButton = resultView.findViewById(R.id.exitButton);
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+        double calc = 0.6* quizQuestions.size();
+
+        // If the user scored 100%, show the trophy and confetti animation
+        if (score >= calc) {
+            // Set the visibility of the trophy FrameLayout to visible
+            trophyFrameLayout.setVisibility(View.VISIBLE);
+
+            /*// Start the confetti animation
+            new ConfettiManager(context, confettoGenerator, confettiSource, container)
+                    .setEmissionDuration(1000)
+                    .setEmissionRate(100)
+                    .setVelocityX(20, 10)
+                    .setVelocityY(100)
+                    .setRotationalVelocity(180, 180)
+                    .animate();*/
+        }
+
     }
+
+    /*private int calculateScore() {
+        int score = 0;
+        for (int i = 0; i < quizQuestions.size(); i++) {
+            if (userAnswers.get(i) == quizQuestions.get(i).getAnswer()) {
+                score++;
+            }
+        }
+        return score;
+    }*/
 
     private void restartQuiz() {
         score = 0;
