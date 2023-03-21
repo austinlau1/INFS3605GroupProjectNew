d0d62c4658ed52aa07719f6daceb26ce06af75da
RohitW02 <z5310030@ad.unsw.edu.au>
2023-03-21 17:41:32 +1100

    Test Code for Plant Details

--- app/src/main/java/com/example/infs3605groupprojectnew/PlantDetails.java
+++ app/src/main/java/com/example/infs3605groupprojectnew/PlantDetails.java
@@ -1,12 +1,8 @@
 package com.example.infs3605groupprojectnew;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,27 +10,31 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 //import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
+//import com.google.firebase.storage.FirebaseStorage;
+//import com.google.firebase.storage.StorageReference;
 
-import java.util.Locale;
-import java.util.concurrent.Executors;
 
 public class PlantDetails extends AppCompatActivity {
     DatabaseReference database;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.individual_plant_page);
+
+        TextView id = findViewById(R.id.plant_id);
+        TextView plant_name = findViewById(R.id.plantName);
+        TextView scientific_name = findViewById(R.id.scientific_name);
+        TextView geographic_distribution = findViewById(R.id.geographic_distribution);
+        TextView plant_description = findViewById(R.id.plant_description);
+        ImageView plant_image = findViewById(R.id.plant_image);
+        ImageView plant_map = findViewById(R.id.plant_map);
+        Button learn_more_bt = findViewById(R.id.learn_more_bt);
 
         database = FirebaseDatabase.getInstance().getReference("Plants Database");
 
