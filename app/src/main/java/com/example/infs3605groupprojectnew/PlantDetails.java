97cca3a7ba96e7b0f858c833f8e48b005c2d980c
RohitW02 <z5310030@ad.unsw.edu.au>
2023-03-21 17:27:29 +1100

    Plant Details
    Changed some code to pull the information

--- app/src/main/java/com/example/infs3605groupprojectnew/PlantDetails.java
+++ app/src/main/java/com/example/infs3605groupprojectnew/PlantDetails.java
@@ -13,7 +13,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.bumptech.glide.Glide;
+//import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
@@ -36,167 +36,22 @@ public class PlantDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.individual_plant_page);
 
-        // Get reference to the database
         database = FirebaseDatabase.getInstance().getReference("Plants Database");
 
-        TextView id = findViewById(R.id.plant_id);
-        TextView plant_name = findViewById(R.id.plantName);
-        TextView scientific_name = findViewById(R.id.scientific_name);
-        TextView geographic_distribution = findViewById(R.id.geographic_distribution);
-        TextView plant_description = findViewById(R.id.plant_description);
-        ImageView plant_image = findViewById(R.id.plant_image);
-        ImageView plant_map = findViewById(R.id.plant_map);
-        Button learn_more_but = findViewById(R.id.learn_more_but);
-
-        //Firebase storage for image
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference storageRef = storage.getReference().child("images/plant 3.jpeg");
-
-        // Select picture by plant name
-        // StorageReference storageRef = storage.getReference().child(""images/"" + plant.getName() + ".jpg")
-
-        storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-            @Override
-            public void onSuccess(Uri uri) {
-                // Load the image into an ImageView
-                ImageView imageView = findViewById(R.id.plant_image);
-                Glide.with(getApplicationContext())
-                        .load(uri.toString())
-                        .into(imageView);
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception exception) {
-                // Handle any errors
-            }
-        });
-
-        /*learn_more_but.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent google_search = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/search?q=" + plant.getName()));
-                // Intent google_search = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/search?q=eth&rlz=1C5CHFA_enHK1019HK1019&oq=eth+&aqs=chrome..69i57j0i67j0i67i131i433j0i67i433j0i67j69i65j69i61l2.1874j1j7&sourceid=chrome&ie=UTF-8"));
-                startActivity(google_search);
-            }
-        });*/
-
         Intent intent = getIntent();
-        String plantId = intent.getStringExtra(PlantList.PLANT_SYMBOL_KEY);
+        String plantSymbol = intent.getStringExtra(PlantList.PLANT_SYMBOL_KEY);
 
-        /*Executors.newSingleThreadExecutor().execute(new Runnable() {
+        database.child(plantSymbol).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void run() {
-                Plant plant = database.
-                runOnUiThread(new Runnable() {
-                                  @Override
-                                  public void run() {
-                                      if (plant != null) {
-                                          id.setText(plant.getId());
-                                          plant_name.setText(plant.getName());
-                                          scientific_name.setText(plant.getScientificName());
-                                          geographic_distribution.setText(plant.getGeographicDistribution());
-                                          plant_description.setText(plant.getGeographicDistribution());
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Plant plant = snapshot.getValue(Plant.class);
+                // Use the plant data to populate the UI elements in the layout XML file
+            }
 
-                                          database.addValueEventListener(new ValueEventListener() {
-                                              @Override
-                                              public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                                  // Handle data changes
-                                                  // Use getValue() to get the data
-                                              }
-
-                                              @Override
-                                              public void onCancelled(@NonNull DatabaseError error) {
-                                                  // Handle errors
-                                              }
-                                          });
-
-                                          learn_more_but.setOnClickListener(new View.OnClickListener() {
-                                              @Override
-                                              public void onClick(View view) {
-                                                  Intent google_search = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/search?q=" + plant.getName()));
-                                                  // Intent google_search = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/search?q=eth&rlz=1C5CHFA_enHK1019HK1019&oq=eth+&aqs=chrome..69i57j0i67j0i67i131i433j0i67i433j0i67j69i65j69i61l2.1874j1j7&sourceid=chrome&ie=UTF-8"));
-                                                  startActivity(google_search);
-                                              }
-                                          });
-
-                                      }
-                                  }
-                              }*/
-
-
-        // mDb =
-
-       /* Intent intent = getIntent();
-        String plantId = intent.getStringExtra(PlantFragment.PLANT_SYMBOL_KEY);
-
-        Executors.newSingleThreadExecutor().execute(new Runnable() {
             @Override
-            public void run() {
-                Plant plant = databaseReference.getPlant(plantId);
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (plant != null) {
-                            id.setText(plant.getId());
-                            coin_symbol_text.setText(plant.getSymbol());
-                            value_text.setText(coin.getPriceUsd());
-                            change1h_text.setText(coin.getPercentChange1h());
-                            change24h_text.setText(coin.getPercentChange24h());
-                            change7d_text.setText(coin.getPercentChange7d());
-                            marketCap_text.setText(coin.getMarketCapUsd());
-                            volume_text.setText(String.format("%.2f", coin.getVolume24()));
-
-                            myRef.addValueEventListener(new ValueEventListener() {
-                                @Override
-                                public void onDataChange(DataSnapshot dataSnapshot) {
-                                    // This method is called once with the initial value and again
-                                    // whenever data at this location is updated.
-                                    String value = dataSnapshot.getValue(String.class);
-                                    if(value != null){
-                                        if (value.equals(coin.getSymbol())){
-                                            favorite_checkbox.setChecked(true);
-                                        }
-                                        Log.d("CoinDetailActivity", "Value is: " + value);
-                                    }
-
-                                }
-
-                                @Override
-                                public void onCancelled(DatabaseError error) {
-                                    // Failed to read value
-                                    Log.w("CoinDetailActivity", "Failed to read value.", error.toException());
-                                }
-                            });
-
-                            learn_more_but.setOnClickListener(new View.OnClickListener() {
-                                @Override
-                                public void onClick(View view) {
-                                    Intent google_search = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/search?q=" + plant.getName()));
-                                    startActivity(google_search);
-                                }
-                            });
-                        }
-
-                        *//*String url = "https://www.coinlore.com/img/" + coin.getNameid().toLowerCase(Locale.ROOT) + ".png";
-
-                        Glide.with(DetailActivity.this)
-                                .load(url)
-                                .centerCrop()
-                                .override(300, 300)
-                                //.placeholder(R.drawable.loading)
-                                .into(coin_image);
-
-                        // Read from the database
-
-                    }
-
-                });*//*
-
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Handle any errors that occur while querying the database
             }
         });
-
-    }
-}*/
-
     }
 }
