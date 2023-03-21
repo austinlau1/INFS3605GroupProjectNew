b253b31b9fe3538b55717e77ffb42944c7e4719c
RohitW02 <z5310030@ad.unsw.edu.au>
2023-03-21 17:57:24 +1100

    Update PlantDetails.java

--- app/src/main/java/com/example/infs3605groupprojectnew/PlantDetails.java
+++ app/src/main/java/com/example/infs3605groupprojectnew/PlantDetails.java
@@ -9,14 +9,15 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+//import com.google.firebase.storage.FirebaseStorage;
+//import com.google.firebase.storage.StorageReference;
 //import com.bumptech.glide.Glide;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-//import com.google.firebase.storage.FirebaseStorage;
-//import com.google.firebase.storage.StorageReference;
+
 
 
 public class PlantDetails extends AppCompatActivity {
