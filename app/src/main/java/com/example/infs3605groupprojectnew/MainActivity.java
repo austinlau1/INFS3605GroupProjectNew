3d5152d961d02386cf4ae0d6fdc066cba2cabe61
annanana164 <thquynh305@gmail.com>
2023-04-01 18:24:22 +1100

    Finished Map Fragement UI

--- app/src/main/java/com/example/infs3605groupprojectnew/MainActivity.java
+++ app/src/main/java/com/example/infs3605groupprojectnew/MainActivity.java
@@ -6,8 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private TextView mDate, mUserDisplay;
+
+    private Calendar mCalendar;
+
+    private SimpleDateFormat mDateFormat;
+    private String date;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button investingButton = (Button) findViewById(R.id.menuButton);
+        mDate = (TextView) findViewById(R.id.date_display);
+
+
 
         investingButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,6 +39,10 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+        //display current date and time
+        mCalendar = Calendar.getInstance();
+        mDateFormat = new SimpleDateFormat("EEEE, MMMM-dd");
+        date = mDateFormat.format(mCalendar.getTime());
+        mDate.setText(date);
     }
 }
